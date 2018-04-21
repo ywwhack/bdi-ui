d5e1f777925cf3b640bae91802037c2fa720bf8a
ywwhack <yww7289@163.com>
2018-04-21 19:35:51 +0800

    feat: wip

--- examples/App.vue
+++ examples/App.vue
@@ -1,17 +1,53 @@
 <template>
   <div id="app">
-    <bdi-index-filter-doc></bdi-index-filter-doc>
+    <bdi-index-filter-demo></bdi-index-filter-demo>
+    <bdi-index-filter :data="options" :selected.sync="selected"></bdi-index-filter>
   </div>  
 </template>
 
 <script>
 import Vue from 'vue'
+import { IndexFilter } from 'bdi-ui'
 import indexFilterDoc from './docs/index-filter.md'
-const IndexFilter = Vue.extend(indexFilterDoc)
+const IndexFilterDemo = Vue.extend(indexFilterDoc)
 
 export default {
+  data () {
+    return {
+      options: [
+        {
+          'id': 1,
+          'name': '交易额',
+          'type': '常规'
+        },
+        {
+          'id': 2,
+          'name': '有效订单量',
+          'type': '常规'
+        },
+        {
+          'id': 3,
+          'name': '实付金额',
+          'type': '常规'
+        },
+        {
+          'id': 4,
+          'name': '产品收入',
+          'type': '常规'
+        },
+        {
+          'id': 5,
+          'name': '下单用户数',
+          'type': '常规'
+        }
+      ],
+      selected: []
+    }
+  },
+
   components: {
-    'bdi-index-filter-doc': IndexFilter
+    'bdi-index-filter-demo': IndexFilterDemo,
+    BdiIndexFilter: IndexFilter
   }
 }
 </script>
