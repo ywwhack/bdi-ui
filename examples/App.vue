bccf0d51e6d1ea52c9dc40e000cc54855c855d81
ywwhack <yww7289@163.com>
2018-04-22 14:45:53 +0800

    feat: intergrated to faas

--- examples/App.vue
+++ examples/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <bdi-index-filter-doc></bdi-index-filter-doc>
+    <bdi-index-filter-demo></bdi-index-filter-demo>
   </div>  
 </template>
 
 <script>
 import Vue from 'vue'
-import indexFilterDoc from './docs/index-filter.md'
-const IndexFilter = Vue.extend(indexFilterDoc)
+import IndexFilterMd from './docs/index-filter.md'
+const BdiIndexFilterDemo = Vue.extend(IndexFilterMd)
 
 export default {
   components: {
-    'bdi-index-filter-doc': IndexFilter
+    BdiIndexFilterDemo
   }
 }
 </script>
--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import BdiUI from '../src'
 import DemoBlock from './components/DemoBlock'
 
