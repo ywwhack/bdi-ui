1f7776e8eca745d4960e83bd29905a0d49f805a7
ywwhack <yww7289@163.com>
2018-04-22 14:18:07 +0800

    feat: add build examples script

---
+++ config/examples.prod.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
+const config = require('./base')
+const utils = require('./utils')
+
+function resolve (name) {
+  return path.resolve(__dirname, '..', name)
+}
+
+module.exports = merge(config, {
+  mode: 'production',
+  entry: {
+    app: resolve('examples/main.js')
+  },
+  output: {
+    path: resolve('dist'),
+    filename: 'js/[name].[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: utils.cssLoaders({ extract: true })
+        }
+      },
+      ...utils.styleLoaders({ extract: true }),
+      ...utils.mdLoaders()
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[chunkhash].css'
+    }),
+    new CleanPlugin(['dist'], { root: path.resolve(__dirname, '..') }),
+    new HtmlPlugin({
+      template: resolve('examples/index.html')
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  },
+  stats: {
+    all: undefined,
+    assets: true,
+    modules: false,
+    children: false,
+    warnings: false
+  }
+})
