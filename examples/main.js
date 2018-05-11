be076c7ffd2b77a494d337c060e600014a6be8d9
ywwhack <yww7289@163.com>
2018-05-11 12:17:00 +0800

    feat(docs): add sidber doc

--- examples/main.js
+++ examples/main.js
@@ -7,12 +7,14 @@ import DemoBlock from './components/DemoBlock'
 
 import BdiIndexFilter from '../src/components/IndexFilter'
 import BdiLogicSelector from '../src/components/LogicSelector'
+import BdiSidebar from '../src/components/Sidebar'
 
 Vue.use(ElementUI)
 
 Vue.component('demo-block', DemoBlock)
 Vue.component('bdi-index-filter', BdiIndexFilter)
 Vue.component('bdi-logic-selector', BdiLogicSelector)
+Vue.component('bdi-sidebar', BdiSidebar)
 
 new Vue({
   el: '#app',
