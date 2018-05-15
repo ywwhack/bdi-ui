1cf484869a89400f4eeca597a355cbaaf1f7714e
ywwhack <yww7289@163.com>
2018-05-15 17:55:21 +0800

    fix(lib): add loading to lib components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import './styles/index.scss'
 import IndexFilter from './components/IndexFilter'
 import LogicSelector from './components/LogicSelector'
 import Sidebar from './components/Sidebar'
+import Loading from './components/Loading'
 
 const components = [
   IndexFilter,
   LogicSelector,
-  Sidebar
+  Sidebar,
+  Loading
 ]
 
 export default function installBdiUI (Vue) {
